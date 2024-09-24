b139d11c6cc50309818c4a001f4f880eaac45382
Miguel Ramos <return.infox@gmail.com>
2024-09-24 20:55:37 -0300

    actualizado

--- PruebaFormularioProject/PruebaFormulario.cs
+++ PruebaFormularioProject/PruebaFormulario.cs
@@ -1,5 +1,6 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace PruebaFormularioProject
 {
@@ -7,15 +8,20 @@ namespace PruebaFormularioProject
     public class PruebaFormulario
     {
         private IWebDriver driver; // Driver para controlar el navegador
+        private WebDriverWait wait; // Espera explícita
         [SetUp]
         public void SetUp()
         {
             // Método que se ejecuta antes de cada test
             driver = new ChromeDriver();
             driver.Manage().Window.Maximize(); // Maximizar la ventana para tener una mejor visualización
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10); // Espera implícita de 10 segundos
             //Navegar a la página de la aplicación
             driver.Navigate().GoToUrl("https://seleniumtestweb.netlify.app/");
 
+            // Creamos una instancia para esperas explícitas
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
         }
 
         [TearDown]
@@ -37,6 +43,27 @@ namespace PruebaFormularioProject
             inputTexto.Clear(); // Limpiar el campo
             inputTexto.SendKeys("Sofia"); // Escribir un texto
 
+            // Encontrar el campo email por su ID y escribimos un email válido
+            IWebElement inputEmail = driver.FindElement(By.Id("input-email")); // Campo Email
+            inputEmail.Clear(); // Limpiar el campo
+            inputEmail.SendKeys("sofia@selenium.com"); // Escribir un email
+
+            // Encontrar el campo de contraseña por su ID y escribimos una contraseña válida
+            IWebElement inputPassword = driver.FindElement(By.Id("input-password")); // Campo Contraseña
+            inputPassword.Clear(); // Limpiar el campo
+            inputPassword.SendKeys("secretoabsoluto"); // Escribir un email
+
+            // Seleccionamos una opción valida del select
+            SelectElement selectOpciones = new SelectElement(driver.FindElement(By.Id("select-opciones")));
+            selectOpciones.SelectByValue("opcion1");
+
+            // Seleccionamos un radio button
+            IWebElement radio1 = driver.FindElement(By.Id("radio1")); // Radio button 1
+            radio1.Click(); // Seleccionar el radio button 1
+
+            // Marcar el checkbox requerido
+            IWebElement checkbox = driver.FindElement(By.Id("checkbox1")); // Checkbox 1
+            checkbox.Click(); // Marcar el checkbox 1
         }
     }
 }
